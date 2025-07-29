20be491c00cc0c897820753cb7e4aff80d3b53aa
Elarin <elarinhh@gmail.com>
2025-07-29 15:08:10 +0900

    fix: settings에 apps 추가

--- be/glossy/settings.py
+++ be/glossy/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # package
+    'rest_framework',
+    'corsheaders',
+    'django_extensions',
+    # apps
+    'accounts',
+    'glossymatcha',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
