6ea7bdeb08b44acfd5143ff09133be0e5ae41fb1
Elarin <elarinhh@gmail.com>
2025-07-30 10:29:46 +0900

    test

--- be/glossymatcha/views.py
+++ be/glossymatcha/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def health_check(request):
+    """헬스체크 엔드포인트"""
+    return Response({
+        'status': 'ok',
+        'timestamp': datetime.now().isoformat()
+    }, status=status.HTTP_200_OK)
