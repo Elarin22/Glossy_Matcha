aa3492afb95bf09bfef6430f318401a02f1be994
Elarin <elarinhh@gmail.com>
2025-07-31 16:34:20 +0900

    [BE] Django ORM 구현 및 문의하기 API 구현(DB저장용)

--- be/glossymatcha/urls.py
+++ be/glossymatcha/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 헬스체크 엔드포인트
     path('health/', views.health_check, name='health_check'),
+    
+    # 문의하기 API 엔드포인트
+    path('inquiries/', views.create_inquiry, name='create_inquiry'),           # POST: 문의 생성
+    path('inquiries/list/', views.get_inquiries, name='get_inquiries'),        # GET: 전체 문의 목록 조회
+    path('inquiries/<int:pk>/', views.get_inquiry, name='get_inquiry'),        # GET: 특정 문의 상세 조회
 ]
