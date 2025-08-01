c0ed4060d5992e728346d7a83dae7ad184af7ddb
Kim Yu-Bin <han6wol@gmail.com>
2025-08-01 20:21:44 +0900

    feat: Footer 동작 확인 완료 Nav 수정 필요

---
+++ fe/src/components/Nav/Nav.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import styles from './Nav.module.scss';
+
+interface MenuItem {
+    label: string;
+    href: string;
+}
+
+interface NavProps {
+    menuItems: MenuItem[];
+}
+
+export function Nav({ menuItems }: NavProps) {
+    const pathname = usePathname();
+
+    return (
+    <nav className={styles.nav}>
+        <ul className={styles.menuList}>
+        {menuItems.map((item) => (
+            <li key={item.href}>
+            <Link
+                href={item.href}
+                className={`${styles.menuItem} ${
+                pathname === item.href ? styles.active : ''
+                }`}
+            >
+                {item.label}
+            </Link>
+            </li>
+        ))}
+        </ul>
+    </nav>
+    );
+}
+
+// ********* 사용예시(브랜드 소개) *********
+// const mainMenuItems = [
+//     { label: '브랜드 소개', href: '/brand' },
+//     { label: '브랜드 철학', href: '/philosophy' },
+//     { label: '제주 유기농 말차', href: '/matcha' },
+//     { label: '연혁', href: '/history' }
+// ];
+
+// <Nav menuItems={mainMenuItems} />
+
+// ********* 사용예시(제품) *********
+// const productMenuItems = [
+//   { label: '시그니처', href: '/signature' },
+//   { label: '말차다구세트', href: '/tea-set' },
+//   { label: '틴케이스', href: '/tin-case' }
+// ];
+
+// <Nav menuItems={productMenuItems} />
