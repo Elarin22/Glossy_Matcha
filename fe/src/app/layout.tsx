329d98c3c60c21c1aa3ffc2d8ca9614c220d6a62
chohc <68624769+chohc@users.noreply.github.com>
2025-08-01 22:55:06 +0900

    [FE] 헤더 완성 (#64)
    머지 후 헤더 푸시했습니다

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,15 +1,21 @@
-import  Footer  from '@/components/Footer/Footer'; 
+import "@/styles/reset.scss";
+import "@/styles/globals.scss";
+import "@/styles/fonts.scss";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="ko">
-            <body>{children}
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1>글로시 말차 페이지</h1>
-    </main>
+    <>
+      <main>
+        <h1>글로시 말차 페이지</h1>
+      </main>
+    </>
   );
 }
