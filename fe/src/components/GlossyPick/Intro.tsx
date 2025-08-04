d39c74a1425bcf52530699932748b4f458a499b0
seonghyeon.kim <gogo20sh@gmail.com>
2025-08-04 15:58:01 +0900

    [FE] style: 체험현 콘텐츠 메인 페이지 스타일링 - 배너 추가, 헤더 분리, scss 추가

--- fe/src/components/GlossyPick/Intro.tsx
+++ fe/src/components/GlossyPick/Intro.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import Image from "next/image";
+import GlossyPickHeader from "./GlossyPickHeader";
+import styles from "./Intro.module.scss";
+
 export default function Intro() {
     return (
-        <>
-            <header>
-                <h1>Glossy Pick</h1>
-                <p>단 하나, 당신만을 위한 글로시 말차</p>
-            </header>
-            <p>몇가지 질문에 답하면 메뉴를 추천해드려요</p>
-            <img
+        <div className={styles.intro}>
+            <GlossyPickHeader />
+            <p className={styles["intro__description"]}>
+                몇 가지 질문에 답하면, 당신에게 어울리는 글로시 말차 메뉴를
+                추천해드려요.
+            </p>
+            <Image
+                className={styles["intro__image"]}
                 src="/images/glossy-pick/intro.svg"
                 alt="글로시 말차 메뉴 이미지"
+                width={970}
+                height={420}
             />
-            <button>시작하기</button>
-        </>
+            <button className="btn-g pc">시작하기</button>
+        </div>
     );
 }
