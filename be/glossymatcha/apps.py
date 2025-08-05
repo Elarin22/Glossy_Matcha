4e4d978219987f5874797852c6d4854052b9745b
Elarin <elarinhh@gmail.com>
2025-08-05 16:04:44 +0900

    [BE] feat: 신호 로드 model 임포트 함수 추가

--- be/glossymatcha/apps.py
+++ be/glossymatcha/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GlossymatchaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'glossymatcha'
+    
+    def ready(self):
+        import glossymatcha.models  # 신호 로드 models 임포트
