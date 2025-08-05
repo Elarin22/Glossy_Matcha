b996329dc6bd4725ece9816f2500b9b68c8e929c
Elarin <elarinhh@gmail.com>
2025-08-05 16:08:37 +0900

    [BE] feat: root, accounts/ url setup

--- be/glossy/urls.py
+++ be/glossy/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('glossymatcha.urls')),
+    path('', include('glossymatcha.urls')),                 # 템플릿 뷰 + API (루트 경로)
+    path('accounts/', include('django.contrib.auth.urls')), # 로그인/로그아웃
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
