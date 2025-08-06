7176c2e46d07ba53adf73fe05c8f9bef44bb6ada
seonghyeon1022 <129020554+seonghyeon1022@users.noreply.github.com>
2025-08-06 23:20:54 +0900

    [FE] feat: 체험형 결과 컴포넌트 구현 (#136)
    - ResultSection 컴포넌트 구현
    - MatchaGenerator에 ResultSection 컴포넌트 등록
    - ResultSection 컴포넌트 스타일링
    - ResultSection 컴포넌트 반응형 스타일링 구현 (미디어쿼리)
    - 메뉴 이미지 추가
    - GlossyPickHeader 스타일링 수정
    
    ---------
    
    Co-authored-by: seonghyeon.kim <gogo20sh@gmail.com>

---
+++ fe/src/components/GlossyPick/ResultSection.tsx
@@ -0,0 +1,61 @@
+import { MenuInfo } from "@/types/matcha";
+import Image from "next/image";
+import styles from "./ResultSection.module.scss";
+import GlossyPickHeader from "./GlossyPickHeader";
+
+interface ResultSectionProps {
+    menuInfo: MenuInfo;
+    onShare: () => void;
+    onDownload: () => void;
+    onReset: () => void;
+}
+
+export default function ResultSection({
+    menuInfo,
+    onShare,
+    onDownload,
+    onReset,
+}: ResultSectionProps) {
+    return (
+        <div id="result-section" className={styles.result}>
+            <GlossyPickHeader />
+            <p className={styles.result__title}>
+                글로시 말차가 제안하는 당신만을 위한 한 잔
+            </p>
+
+            <section className={styles["result__menu"]}>
+                <h4 className="sr-only">글로시 메뉴 추천</h4>
+                <Image
+                    className={styles["result__image"]}
+                    src={menuInfo.image}
+                    alt={`${menuInfo.name} 이미지`}
+                    width={230}
+                    height={360}
+                />
+                <p className={styles["result__menuName"]}>{menuInfo.name}</p>
+                <div className={styles.result__tags}>
+                    {menuInfo.tags.map((tag, index) => (
+                        <span key={index} className="tag">
+                            {tag}
+                        </span>
+                    ))}
+                </div>
+                <p className={styles.result__menuDescription}>
+                    {menuInfo.description}
+                </p>
+            </section>
+
+            <div className={styles.result__actions}>
+                <button onClick={onShare} className="btn-g">
+                    공유하기
+                </button>
+                <button onClick={onDownload} className="btn-g">
+                    저장하기
+                </button>
+                <button onClick={onReset} className="btn-g">
+                    다시하기
+                </button>
+            </div>
+        </div>
+    );
+}
