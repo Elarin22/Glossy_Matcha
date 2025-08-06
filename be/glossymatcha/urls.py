f841280ea595cd55153425a5ffe53c0f87d8e46c
Elarin <elarinhh@gmail.com>
2025-08-06 12:02:44 +0900

    [BE] feat: Create Dashboard urls

--- be/glossymatcha/urls.py
+++ be/glossymatcha/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 문의하기 API 엔드포인트
-    path('inquiries/', views.create_inquiry, name='create_inquiry'),           # POST: 문의 생성
-    path('inquiries/list/', views.get_inquiries, name='get_inquiries'),        # GET: 전체 문의 목록 조회
-    path('inquiries/<int:pk>/', views.get_inquiry, name='get_inquiry'),        # GET: 특정 문의 상세 조회
+    # Django Template를 사용한 대시보드 뷰
+    path('', views.DashboardView.as_view(), name='dashboard'),
 ]
