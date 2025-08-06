2ba684bd5cc545a0a5c73931b7771ef97389ad92
seonghyeon.kim <gogo20sh@gmail.com>
2025-08-06 21:06:31 +0900

    [FE] feat: Intro 컴포넌트 등록

--- fe/src/components/GlossyPick/MatchaGenerator.tsx
+++ fe/src/components/GlossyPick/MatchaGenerator.tsx
@@ -1,10 +1,43 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./MatchaGenerator.module.scss";
+
+import { questions } from "@/data/questions";
+import { menuData } from "@/data/menuData";
+import { useMatchaQuiz } from "@/hooks/useMatchaQuiz";
+import { useShare } from "@/hooks/useShare";
+import { useDownload } from "@/hooks/useDownload";
 import Intro from "./Intro";
+import styles from "./MatchaGenerator.module.scss";
 
 export default function MatchaGenerator() {
+    const {
+        currentStep,
+        answers,
+        handleAnswer,
+        resetQuiz,
+        startQuiz,
+        recommendation,
+    } = useMatchaQuiz();
+
+    const { shareResult } = useShare();
+
+    const { downloadImage } = useDownload();
+
+    const handleShare = () => {
+        if (!recommendation) return;
+        shareResult(recommendation);
+    };
+
+    const handleDownload = () => {
+        if (recommendation) {
+            downloadImage(
+                "result-section",
+                `${menuData[recommendation].name}.png`
+            );
+        }
+    };
+
     return (
         <main className={styles["matcha-generator"]}>
             <section>
@@ -19,7 +52,7 @@ export default function MatchaGenerator() {
                 />
             </section>
             <section className={styles["matcha-generator__section"]}>
-                <Intro />
+                {currentStep === 0 && <Intro onStart={startQuiz} />}
             </section>
         </main>
     );
