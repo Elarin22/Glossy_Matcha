4501bbc6b9ddd5094a7b7156b5dd699fd83b5cc6
chohc <68624769+chohc@users.noreply.github.com>
2025-08-08 10:42:47 +0900

    [FE] 홈 반응형 1차 완성 (#167)
    대공사 했어요😎
    오류있거나 디자인적 제안사항 있으면 알려주세요! 
    
    - 모바일(768px) 감지 훅 useMobileDetect 추가 (Header.tsx에서 분리)
    - 모바일 홈 1차 완성
       -  PC 홈과 Mobile 홈을 컴포넌트로 분리 => Home에서는 isMobile에 따라 분기만
       - 🚧 맨 밑에 Footer 도착하고 다시 위로 올릴때 Footer가 사라지지 않는 문제있음
       - 🚧 모바일버전에서 0.2초정도 PC 버전이 보임 (isMobile이 랜더링 되고 세팅됨)
    - 모바일 Header 사이드바 메뉴들 간격 수정 (gap: 1.25rem -> 1.6rem)
    - ImageSection, SideInfo로 흩어져있던 정보들을 Home에 HomeContents로 모음 (확장성, 유지보수성)
    - SoundButton 컴포넌트 추가 (ImageSection에서 분리)
    - ScrollIndicator 수정
       - 스타일 수정
       - 매개변수 bottom 추가 (position bottom값, default => 80)

---
+++ fe/src/components/Home/PcHome/PcHome.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import styles from "./PcHome.module.scss";
+import { useEffect, useRef, useState } from "react";
+import SideInfo from "@/components/Home/SideInfo/SideInfo";
+import ImageSection from "@/components/Home/ImageSection/ImageSection";
+import { HomeContent } from "@/app/page"; // 혹은 props로 받기
+
+export default function PcHome({ contents }: { contents: HomeContent[] }) {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const sectionRefs = useRef<HTMLDivElement[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const index = sectionRefs.current.findIndex(
+              (el) => el === entry.target
+            );
+            if (index !== -1) setCurrentIndex(index || 0);
+          }
+        });
+      },
+      { threshold: 0.6 }
+    );
+
+    sectionRefs.current.forEach((el) => {
+      if (el) observer.observe(el);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <main className={styles["pc-container"]}>
+      <SideInfo currentIndex={currentIndex} contents={contents} />
+      <ImageSection sectionRefs={sectionRefs} contents={contents} />
+    </main>
+  );
+}
---
+++ fe/src/components/Home/SoundButton/SoundButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import styles from "./SoundButton.module.scss";
+
+export default function SoundButton({
+  videoRef,
+}: {
+  videoRef: React.RefObject<HTMLVideoElement | null>;
+}): React.JSX.Element {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = useCallback(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = !video.muted;
+      setIsMuted(video.muted);
+    }
+  }, []);
+
+  return (
+    <button
+      onClick={toggleMute}
+      className={styles["sound-toggle-button"]}
+      aria-label={isMuted ? "소리 켜기" : "소리 끄기"}
+    >
+      {isMuted ? (
+        <img src={"/images/icon/sound-off.svg"} alt="" />
+      ) : (
+        <img src={"/images/icon/sound-on.svg"} alt="" />
+      )}
+    </button>
+  );
+}
