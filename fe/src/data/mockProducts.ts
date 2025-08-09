a0fd9b01d488d50b807d680101f62ed231e213c8
6wol <han6wol@gmail.com>
2025-08-09 22:38:53 +0900

    [FE]mockProducts 임시 추가 (#214)
    - mid banner 글자 상하 간격 수정
    - api 연동 전 사용할 mockProducts.ts 추가 (연동 시 삭제 예정)
    - 상세 사진 추가

---
+++ fe/src/data/mockProducts.ts
@@ -0,0 +1,110 @@
+export const mockSignature = {
+    id: 0,
+    name: "시그니처", 
+    subtitle: "100% 제주 새봄의 첫 순을 담은 부드러운 말차 포켓",  
+    description: "세레모니얼 말차 | 대나무 수액 | 코코넛슈가 제주산 세레모니얼 등급 말차 원료와 대체당, 대나무수액, 코코넛슈가를 블렌딩한 은은한 단맛의 밸런스",  
+    short_description: "글로시말차 시그니처는 실제 매장에서 사용되는 말차와 동일한 원료로 제작된 상품입니다.", 
+    mid_banner_img: "||BANNER:/images/product/signature-mid-banner.jpg",
+
+    sub_descriptions: [
+    {
+        id: 0,
+        sub_description: "말차, 이제 쉽고 간편하게 즐겨요||귀찮고 복잡한 차선과 차완 대신, 오직 10g. 단 한 포로 손쉽게 말차 음료를 완성하세요.",
+        images: [
+        { image: "/images/product/signature1.jpg" }
+        ]
+    },
+    {
+        id: 1,
+        sub_description: "제주의 봄, <br>첫순으로만 만든 유기농 말차||국내에는 세레모니얼 등급의 인증기관이 없는 대신,<br>글로시말차는 '일본 등급표 기준'을 만족하도록 봄의 '첫 순',<br>1번 잎으로 만든 진짜 세레모니얼 유기농 말차입니다",
+        images: [
+        { image: "/images/product/signature2.jpg" }
+        ]
+    },
+        {
+    id: 2,
+    sub_description: "떫은 맛은 줄이고, <br> 신선한 향을 살린 깔끔한 피니쉬||말차는 고유의 떫고 쓴 맛으로 호불호가 있으나, <br> 글로시말차의 원료는 신선한 원료와 가공 노하우로 떫은 <br> 맛은 적고, 크림처럼 부드러운 텍스쳐와 끝맛이 일품입니다.",
+    images: [
+        { image: "/images/product/signature3.jpg" }
+    ]
+    },
+    {
+    id: 3,
+    sub_description: "자연의 은은한 단 맛을 부담없이||대체당과 코코넛슈가, 대나무수액 원당의 블렌딩으로 <br> 당류 부담은 낮추고, 인위적인 대체당의 맛 대신에 <br>원당의 자연스런 단맛과 풍미를 즐기세요. <br> (1회 섭취량 기준 당류 1g)",
+    images: [
+        { image: "/images/product/signature4.jpg" }
+    ]
+    },
+    {
+    id: 4,
+    sub_description: "휴대성과 간편성은 더 강화한 감각적인 디자인||꽂아두기 쉬운 세로형 스틱과 슬라이드형 패키지. <br> 그리고 이지컷 방식으로 제작된 상단부로 더 편리해요.",
+    images: [
+        { image: "/images/product/signature5.jpg" }
+    ]
+    }
+    ]
+};
+
+export const mockTeaSet = {
+    id: 1,
+    name: "말차다구세트", 
+    subtitle: "차 한 잔의 격을 더하다",  
+    description: "차선 | 사발 | 스픈 | 차탁<br>글로시말차 매장의 감도를 담은 정제된 구성",  
+    mid_banner_img: "||BANNER:/images/product/teacase-mid-banner.jpg",
+
+    sub_descriptions: [
+    {
+    id: 0,
+    sub_description: "차완 | 말차 그릇 ||제주 로컬 아티스트와 협업한 고온소성(800~1200도) 백자 제품입니다. <br> 말차 격불 또는 일반 차 우림에 적합하며, <br> 초보자도 쉽게 사용할 수 있도록 곡선, 용량, 높이 등을 정밀하게 설계했습니다.",
+    images: [
+        { image: "/images/product/teaset1.webp" }
+    ]
+    },
+    {
+    id: 1,
+    sub_description: "차선꽂이 | 차선 거치대 || 차선을 깔끔하게 건조하고 갈래를 잘 유지할 수 있도록 <br> 안정적인 형태로 제작되었습니다.",
+    images: [
+        { image: "/images/product/teaset2.webp" }
+    ]
+    },
+    {
+    id: 2,
+    sub_description: "차선 | 말차용 대나무 거품기 || 100갈래 대나무 차선으로 <br>물과 말차가 잘 섞여 부드럽고 안정적인 거품을 만들어냅니다.",
+    images: [
+        { image: "/images/product/teaset3.webp" }
+    ]
+    },
+    {
+    id: 3,
+    sub_description: "차시 | 말차 긴 스픈 || 국내산대나무로 길고 가는 디자인으로 <br> 말차가루를 보다 쉽고 편하게 덜 수 있습니다.",
+    images: [
+        { image: "/images/product/teaset4.webp" }
+    ]
+    },
+    ]
+};
+
+export const mockCase = {
+    id: 2,
+    name: "틴케이스", 
+    subtitle: "제주의 자연을 담은 100% HIGH QUALITY 유기농 말차",  
+    description: "틴케이스 SET <br> 최상급 말차 50g + 우드스픈 + 틴케이스",  
+    short_description: "* 국산 말차에는 등급제가 없습니다만, 해외 등급표 기준을 만족하도록 첫순 1번 잎을 가지고 만든 세레모니얼 급 '유기농 말차' 입니다.", 
+    mid_banner_img: "||BANNER:/images/product/case-mid-banner.jpg",
+    sub_descriptions: [
+    {
+    id: 0,
+    sub_description: "GRADE || 제주도의 청정자연을 담은 말차농원 <br>말차밭에서 매해 첫 순, 첫 번째 잎으로 생산해 낸 말차를 사용합니다.<br>특별한 말차로 더 드럽고, 더욱 진한 맛을 담아냈습니다.<br>서귀포 생태공원에서 재배된 100% 말차만을 사용합니다. <br>우리는 환경을 생각하는 마음으로 지속 가능한 미래를 그립니다.<br>현지 농부, 장인분들과 함께 더 건강한 내일을 향해 나아가려고 합니다.",
+    images: [
+        { image: "/images/product/case1.webp" }
+    ]
+    },
+    {
+    id: 1,
+    sub_description: " PRICE || 글로시말차는 오래도록 함께할 수 있는 방법을 고민합니다.<br>오랜 노하우로 인한 가공기법과 유통 관리를 통해<br>최고 품질의 말차만을 제공할 것을 약속합니다. <br>시중에 판매중인 일반적인 말차의 판매가보다 <br>훨씬 합리적인 가격에 최상급 원료를 제공합니다. <br>품질에 대한 자부심 그리고 공생할 수 있는 미래를 준비합니다.",
+    images: [
+        { image: "/images/product/case2.webp" }
+    ]
+    }
+    ]
+};
