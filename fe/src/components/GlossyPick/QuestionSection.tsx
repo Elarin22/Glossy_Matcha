281dd1eb3c482679db682811b311e58339b7d464
seonghyeon.kim <gogo20sh@gmail.com>
2025-08-11 12:00:06 +0900

    [FE] feat: 질문 국영문 전환 적용

--- fe/src/components/GlossyPick/QuestionSection.tsx
+++ fe/src/components/GlossyPick/QuestionSection.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { Question, AnswerType } from "@/types/matcha";
 import GlossyPickHeader from "./GlossyPickHeader";
 import styles from "./QuestionSection.module.scss";
@@ -15,11 +16,13 @@ export default function QuestionSection({
     onAnswer,
     onPrev,
 }: QuestionSectionProps) {
+    const t = useTranslations("test.questions");
+
     return (
         <div className={styles.question}>
             <GlossyPickHeader />
 
-            <p className={styles.question__title}>{question.title}</p>
+            <p className={styles.question__title}>{t(question.title)}</p>
 
             <div className={styles.question__options}>
                 {question.options.map((option) => (
@@ -28,7 +31,7 @@ export default function QuestionSection({
                         onClick={() => onAnswer(question.id, option.key)}
                         className="btn-choice"
                     >
-                        {option.text}
+                        {t(option.text)}
                     </button>
                 ))}
             </div>
