b0054bd45946d155e5baf5892e99e43451ef4bf0
seonghyeon1022 <129020554+seonghyeon1022@users.noreply.github.com>
2025-08-11 16:54:20 +0900

    [FE] fix: 체험형 다운로드 기능 제거 (#234)
    ## ios 환경 호환 문제로 인한 다운로드 기능 제거
    - `useDownload` 훅 제거
    
    ## 체험형 css 수정
    - 결과 섹션 `border-radius` 추가
    - 인트로 이미지 크기 수정
    
    ---------
    
    Co-authored-by: seonghyeon.kim <gogo20sh@gmail.com>

--- fe/src/components/GlossyPick/ResultSection.tsx
+++ fe/src/components/GlossyPick/ResultSection.tsx
@@ -7,14 +7,12 @@ import GlossyPickHeader from "./GlossyPickHeader";
 interface ResultSectionProps {
     menuInfo: MenuInfo;
     onShare: () => void;
-    onDownload: () => void;
     onReset: () => void;
 }
 
 export default function ResultSection({
     menuInfo,
     onShare,
-    onDownload,
     onReset,
 }: ResultSectionProps) {
     const tMenu = useTranslations("test.menu");
@@ -55,9 +53,6 @@ export default function ResultSection({
             >
                 <button onClick={onShare} className="btn-g">
                     {tResult("share")}
-                </button>
-                <button onClick={onDownload} className="btn-g">
-                    {tResult("download")}
                 </button>
                 <button onClick={onReset} className="btn-g">
                     {tResult("reset")}
