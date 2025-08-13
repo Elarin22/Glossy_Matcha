8e2e11896b55a824768ad259ce99f2e3b96bdfa1
seonghyeon.kim <gogo20sh@gmail.com>
2025-08-13 15:33:13 +0900

    [FE] feat: 체험형 페이지 메타 데이터 추가

--- fe/src/app/[locale]/test/page.tsx
+++ fe/src/app/[locale]/test/page.tsx
@@ -1,7 +1,26 @@
-"use client";
-
 import MatchaGenerator from "@/components/GlossyPick/MatchaGenerator";
 
+export const metadata = {
+  title: "Glossy Pick | Glossy Matcha",
+  description: "단 하나, 당신만을 위한 글로시 말차",
+  keywords: [
+    "말차",
+    "프리미엄 말차",
+    "글로시 말차",
+    "녹차",
+    "건강음료",
+    "프리미엄",
+    "메뉴 추천",
+    "체험형 콘텐츠",
+    "음료 추천",
+  ],
+  openGraph: {
+    title: "Glossy Pick | Glossy Matcha",
+    description: "단 하나, 당신만을 위한 글로시 말차",
+    url: "https://www.glossymatcha.com/ko/test",
+  },
+};
+
 export default function test() {
-    return <MatchaGenerator />;
+  return <MatchaGenerator />;
 }
