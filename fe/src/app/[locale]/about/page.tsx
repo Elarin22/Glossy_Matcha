2a0bdb61b0a68612f62b83d2d158ae58e71f3af0
seonghyeon.kim <gogo20sh@gmail.com>
2025-08-13 15:35:57 +0900

    [FE] fix: 브랜드 소개 title 수정

--- fe/src/app/[locale]/about/page.tsx
+++ fe/src/app/[locale]/about/page.tsx
@@ -7,7 +7,7 @@ import BrandHistory from "@/components/About/BrandHistory";
 import styles from "./page.module.scss";
 
 export const metadata = {
-  title: "브랜드 소개 | Glossy Matcha",
+  title: "About | Glossy Matcha",
   description:
     "글로시 말차 브랜드 소개 페이지 - 철학, 제주 유기농 말차, 연혁 등을 확인하세요.",
   keywords: [
