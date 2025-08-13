47cb387a59df5727e35bd4623c73a84e032b0ea7
seonghyeon1022 <129020554+seonghyeon1022@users.noreply.github.com>
2025-08-13 17:10:45 +0900

    [FE] feat: useShare 국영문 전환 구현 (#280)
    - `useShare` 국영문 번역문 작성
    - `useShare` 훅에 국영문 전환 구현: `useTranslations` 사용
    
    Co-authored-by: seonghyeon.kim <gogo20sh@gmail.com>

--- fe/src/hooks/useShare.ts
+++ fe/src/hooks/useShare.ts
@@ -1,34 +1,41 @@
+import { useTranslations } from "next-intl";
 import { useCallback } from "react";
 import { toast } from "react-toastify";
 
 export const useShare = () => {
-    const shareResult = useCallback(async (recommendation: string) => {
-        const text = `나의 글로시 말차 추천 메뉴는 "${recommendation}"!`;
-        const url =
-            window.location.origin +
-            window.location.pathname +
-            `?recommendation=${encodeURIComponent(recommendation)}`;
+  const t = useTranslations("share");
 
-        if (navigator.share) {
-            try {
-                await navigator.share({
-                    title: "글로시 말차",
-                    text: text,
-                    url,
-                });
-                toast.success("공유가 완료되었어요!");
-            } catch (error) {
-                toast.error("공유에 실패했어요!");
-            }
-        } else {
-            try {
-                await navigator.clipboard.writeText(text + " " + url);
-                toast.success("링크가 복사되었어요!");
-            } catch (error) {
-                toast.error("링크 복사에 실패했어요!");
-            }
+  const shareResult = useCallback(
+    async (recommendation: string) => {
+      const text = t("shareText") + recommendation + "!";
+
+      const url =
+        window.location.origin +
+        window.location.pathname +
+        `?recommendation=${encodeURIComponent(recommendation)}`;
+
+      if (navigator.share) {
+        try {
+          await navigator.share({
+            title: t("shareTitle"),
+            text: text,
+            url,
+          });
+          toast.success(t("toastShared"));
+        } catch (error) {
+          toast.error(t("toastShareFailed"));
         }
-    }, []);
+      } else {
+        try {
+          await navigator.clipboard.writeText(text + " " + url);
+          toast.success(t("toastCopied"));
+        } catch (error) {
+          toast.error(t("toastCopyFailed"));
+        }
+      }
+    },
+    [t]
+  );
 
-    return { shareResult };
+  return { shareResult };
 };
