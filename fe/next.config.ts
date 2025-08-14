56578b8b9fef22604dcbf1123ac9abd6f7232c46
6wol <han6wol@gmail.com>
2025-08-14 12:21:00 +0900

    [FE]feat: api 주소 수정 (#292)
    변경된 api 주소 코드 수정

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       "localhost",
       "127.0.0.1",
       "api.glossymatcha.com",
-      `${process.env.NEXT_PUBLIC_API_URL}`,
+      "x81fj32kd.glossymatcha.com",
     ],
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
